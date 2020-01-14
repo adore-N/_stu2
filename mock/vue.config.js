f1a11142725dbdd2c9d9929f5c12a3539a41f603
adore-N <819790662@qq.com>
2020-01-14 17:38:42 +0800

    add

---
+++ mock/vue.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+    lintOnSave: false, //禁用eslint
+    // productionSourceMap: false,  // 生产环境的 source map
+    // outputDir:process.env.outputDir, //根据不同环境打包输出不同文件夹
+    // pages:getEntry('./src/pages/**/*.js'),
+    devServer: {
+        // index: "login.html", //默认启动serve 打开login页面
+        open:false, // 执行npm run serve 字动打开浏览器
+        host:'dev.',
+        port: 8089,
+        proxy:{
+            '/v1':{
+                target:'http://localhost:3333',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/v1': '/v1'
+                }
+            }
+        }
+    },
+    // chainWebpack:config=>{
+    //     config.output.filename('[name].[hash:7].js').end();
+    // }
+};
