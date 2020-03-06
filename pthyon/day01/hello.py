186d231835b7d2591dcd9820b3e373c30a04e5f6
adore-N <819790662@qq.com>
2020-03-06 15:04:35 +0800

    1

---
+++ pthyon/day01/hello.py
@@ -0,0 +1,34 @@
+# 输出 print()
+
+# print("hello world")
+# print('https://local.liaoxuefeng.com:39093/')
+
+
+# 输入input()
+# name = input()
+# print("hello",name)
+
+# name = input('please input you name:')
+# print('hello', name)
+
+# 输入是Input，输出是Output，因此，我们把输入输出统称为Input/Output，或者简写为IO。
+
+# age = input("age:")
+# age = int(age)
+# if age < 2000:
+#     print('00')
+# else: 
+#     print('11')
+
+# names = ['111', '222', '333']   
+# for name in names:
+#     print(name)
+
+# ages = (111,222,3444)
+# for age in ages:
+#     print(age)
+
+sum = 0
+for x in range(101):
+    sum = sum + x
+print(sum)
